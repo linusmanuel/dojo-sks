a9e054db696ac88c15f383c00dfccc4772d0dde5
Joao Lino <linusmannuel@gmail.com>
2023-09-10 13:28:08 +0100

    feat: add blog section

--- src/app/components/blog/blog.tsx
+++ src/app/components/blog/blog.tsx
@@ -1,11 +1,34 @@
+'use client'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 import { Card } from '../card/card'
 import './style.css'
+import './swiper.css'
+import 'swiper/css';
 
 export function Blog() {
   return (
     <section className="b-blog">
       <h2 className="b-blog__title">Blog</h2>
-      <Card/>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <Card></Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card></Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card></Card>
+        </SwiperSlide>
+      </Swiper>
     </section>
   )
 }
