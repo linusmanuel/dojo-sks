c932a0e0079f21f6e8548f4f79089cc8ed045274
Joao Lino <linusmannuel@gmail.com>
2023-09-10 13:51:38 +0100

    add partner component

---
+++ src/app/components/partner/partner.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+export function Partner() {
+  return (
+    <section className="b-partner">
+      <h2 className="b-partner__title">Parceiros</h2>
+      <p className="b-partner__text">
+        Contamos com o apoio de nossos parceiros para ensinar a 
+        filosofia do Karate Shotokan em Angola
+      </p>
+      {/* Need to add Slider of Patners */}
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Hero } from "./components/hero/hero"
 import { About } from "./components/about/about";
 import { Gallery } from "./components/gallery/gallery";
 import { Blog } from "./components/blog/blog";
+import { Partner } from "./components/partner/partner";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Gallery />
       <Blog />
+      <Partner/>
     </>
   )
 }
